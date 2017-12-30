0777b9691c3a764c33f1d5c08d8c7ed07f425155
Rob Garrison <wowmotty@gmail.com>
2017-12-30 15:13:48 -0600

    Add `format` option and change the default output to an object (#2)

--- test.js
+++ test.js
@@ -18,15 +18,24 @@ test('rejects', t => {
 	t.is(reject(t, '#123456789'), message);
 });
 
-test('hex', t => {
-	t.deepEqual(hexRgb('4183c4'), [65, 131, 196, 255]);
-	t.deepEqual(hexRgb('#4183c4'), [65, 131, 196, 255]);
-	t.deepEqual(hexRgb('#000'), [0, 0, 0, 255]);
+test('hex; output object', t => {
+	const options = {format: 'object'};
+	t.deepEqual(hexRgb('#4183c4', options), {red: 65, green: 131, blue: 196, alpha: 255});
+	t.deepEqual(hexRgb('#000', options), {red: 0, green: 0, blue: 0, alpha: 255});
+	t.deepEqual(hexRgb('#0008', options), {red: 0, green: 0, blue: 0, alpha: 136});
+	t.deepEqual(hexRgb('4183c488', options), {red: 65, green: 131, blue: 196, alpha: 136});
+	t.deepEqual(hexRgb('#4183c488'), {red: 65, green: 131, blue: 196, alpha: 136});
+	t.deepEqual(hexRgb('4183c4'), {red: 65, green: 131, blue: 196, alpha: 255});
+	t.deepEqual(hexRgb('#000f'), {red: 0, green: 0, blue: 0, alpha: 255});
 });
 
-test('hex with alpha', t => {
-	t.deepEqual(hexRgb('4183c488'), [65, 131, 196, 136]);
-	t.deepEqual(hexRgb('#4183c488'), [65, 131, 196, 136]);
-	t.deepEqual(hexRgb('#0008'), [0, 0, 0, 136]);
-	t.deepEqual(hexRgb('#000f'), [0, 0, 0, 255]);
+test('hex; output array', t => {
+	const options = {format: 'array'};
+	t.deepEqual(hexRgb('4183c4', options), [65, 131, 196, 255]);
+	t.deepEqual(hexRgb('#4183c4', options), [65, 131, 196, 255]);
+	t.deepEqual(hexRgb('#000', options), [0, 0, 0, 255]);
+	t.deepEqual(hexRgb('4183c488', options), [65, 131, 196, 136]);
+	t.deepEqual(hexRgb('#4183c488', options), [65, 131, 196, 136]);
+	t.deepEqual(hexRgb('#0008', options), [0, 0, 0, 136]);
+	t.deepEqual(hexRgb('#000f', options), [0, 0, 0, 255]);
 });
