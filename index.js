a964ac676d7d93a89261f7ca2a617ee1fabbb363
Rob Garrison <wowmotty@gmail.com>
2017-12-30 08:45:14 -0600

    Add hex alpha channel support and require Node.js 6 (#1)

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 'use strict';
+
+const hexChars = 'a-f\\d';
+const match3or4Hex = `#?[${hexChars}]{3}[${hexChars}]?`;
+const match6or8Hex = `#?[${hexChars}]{6}([${hexChars}]{2})?`;
+
+const nonHexChars = new RegExp(`[^#${hexChars}]`, 'gi');
+const validHexSize = new RegExp(`^${match3or4Hex}$|^${match6or8Hex}$`, 'i');
+
 module.exports = function (hex) {
-	if (typeof hex !== 'string') {
-		throw new TypeError('Expected a string');
+	if (typeof hex !== 'string' || nonHexChars.test(hex) || !validHexSize.test(hex)) {
+		throw new TypeError('Expected a valid hex string');
 	}
 
 	hex = hex.replace(/^#/, '');
+	let alpha = 255;
+
+	if (hex.length === 8) {
+		alpha = parseInt(hex.slice(6, 8), 16);
+		hex = hex.substring(0, 6);
+	}
+
+	if (hex.length === 4) {
+		alpha = parseInt(hex.slice(3, 4).repeat(2), 16);
+		hex = hex.substring(0, 3);
+	}
 
 	if (hex.length === 3) {
 		hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
 	}
 
-	var num = parseInt(hex, 16);
-
-	return [num >> 16, num >> 8 & 255, num & 255];
+	const num = parseInt(hex, 16);
+	return [num >> 16, (num >> 8) & 255, num & 255, alpha];
 };
