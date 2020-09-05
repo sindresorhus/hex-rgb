fba1764e96ee1173827268ffca7275b6851f3117
Edgar Goetzendorff <hello@dated.fun>
2020-09-05 23:56:53 +0200

    refactor: update type expectations

--- index.test-d.ts
+++ index.test-d.ts
@@ -2,6 +2,6 @@ import {expectType} from 'tsd';
 import hexRgb = require('.');
 
 expectType<hexRgb.RgbaObject>(hexRgb('#cd2222cc'));
-expectType<hexRgb.RgbaObject | hexRgb.RgbaTuple | string>(hexRgb('#cd2222cc', {format: 'array'}));
-expectType<hexRgb.RgbaObject | hexRgb.RgbaTuple | string>(hexRgb('#cd2222cc', {format: 'css'}));
-expectType<hexRgb.RgbaObject | hexRgb.RgbaTuple | string>(hexRgb('#cd2222cc', {format: 'object'}));
+expectType<hexRgb.RgbaTuple>(hexRgb('#cd2222cc', {format: 'array'}));
+expectType<string>(hexRgb('#cd2222cc', {format: 'css'}));
+expectType<hexRgb.RgbaObject>(hexRgb('#cd2222cc', {format: 'object'}));
