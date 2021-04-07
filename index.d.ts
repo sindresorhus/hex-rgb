ebb0ca06aeaafc2fedc12d0368a75384ee2cc6bd
juyeong1260 <juyeong1260@gmail.com>
2021-04-07 11:12:55 +0900

    sync doc comment with readme

--- index.d.ts
+++ index.d.ts
@@ -10,7 +10,9 @@ declare namespace hexRgb {
 		readonly format?: 'object' | 'array' | 'css';
 
 		/**
-		Note that when 'alpha' option is added, the value of alpha in the hex code is overwritten.
+		Set the alpha of the color.
+
+		This overrides any existing alpha component in the Hex color string. For example, the `99` in `#22222299`.
 
 		This value must be in the range 0 to 1.
 		*/
